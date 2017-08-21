bf8c4a0aa911a687ea897b12b87bb63181815b8b
Seyed Jalal Hosseini <jalalhosseiny@gmail.com>
2017-08-21 03:51:23 +0100

    add /api/ to register and login routes

--- src/main/java/com/artronics/security/WebSecurityConfig.java
+++ src/main/java/com/artronics/security/WebSecurityConfig.java
@@ -28,8 +28,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
                 .antMatchers("/").permitAll()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
-                .antMatchers(HttpMethod.POST, "/register").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/register").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 // We filter the api/login requests
